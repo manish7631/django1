488f95e6a789a650a9d8441b7ec68d982bac4d9f
manish7631 <mk2334994@gmail.com>
2023-05-26 05:23:34 +0530

    add student

--- myapp.py
+++ myapp.py
@@ -6,3 +6,13 @@ URL = ""
 
 
 
+def get_data(id = None):
+ data = {}
+ if id is not None:
+  data = {'id':id}
+ json_data = json.dumps(data)
+ r = requests.get(url = URL, data = json_data)
+ data = r.json()
+ print(data)
+
+get_data(1)
